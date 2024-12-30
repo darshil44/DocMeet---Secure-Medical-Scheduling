1b450763cc1e78971f1238b454a1acb59a6a27ad
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2024-12-30 08:31:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //routes
 //LOGIN || POST
 router.post("/login", loginController);
-
+ 
 //REGISTER || POST
 router.post("/register", registerController);
 module.exports = router;
