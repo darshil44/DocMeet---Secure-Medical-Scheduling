4d11c58e4fbe6f7c82124c8e479613d63bb41b9a
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2025-01-01 12:52:18 +0530

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
  
 const userSchema = new mongoose.Schema({
   name: { 
-    type: String,
+    type: String, 
     required: [true, "name is require"], 
   }, 
   email: {
