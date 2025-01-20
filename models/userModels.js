f217114951937cf5ef47cabf7fdf8049c83403c8
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2025-01-20 16:54:31 +0530

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     type: String,  
     required: [true, "email is require"],
   },
-  password: {
+  password: { 
     type: String,
     required: [true, "password is require"],
   },
